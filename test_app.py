395cd3dfb12519d4d37a7d75a211194ea9c8b7a6
Gabor Szabo <gabor@szabgab.com>
2020-03-31 15:31:40 +0300

    add post test

--- test_app.py
+++ test_app.py
@@ -7,3 +7,9 @@ def test_app():
     assert b'<h1>Welcome to the Flask Demo</h1>' in rv.data
     #print(rv.data)
 
+
+    rv = web.post('/', data = { "text": "FooBar" } )
+    assert rv.status == '200 OK'
+    assert b'<h1>Welcome to the Flask Demo</h1>' in rv.data
+    assert b'<b>FooBar</b>' in rv.data
+
